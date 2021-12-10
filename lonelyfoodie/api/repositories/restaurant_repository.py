b3849b512d9a6fba558b2cf2ec442c92977b100f
Garagekeeper <hj06869@naver.com>
2021-12-10 15:49:48 +0900

    음식점 리뷰개수, 평점 평균 기능 추가
    음식점 리뷰개수, 평점 평균 기능 추가

--- lonelyfoodie/api/repositories/restaurant_repository.py
+++ lonelyfoodie/api/repositories/restaurant_repository.py
@@ -1,8 +1,9 @@
+from sqlalchemy.sql.functions import count
 from werkzeug.exceptions import NotFound
 from lonelyfoodie.api.repositories.repository import Repository
 from lonelyfoodie.database import use_database, Database
 from lonelyfoodie.database.models import Restaurant
-
+import sys
 
 class RestaurantRepository(Repository):
     def __init__(self):
@@ -28,5 +29,55 @@ class RestaurantRepository(Repository):
         restaurant = self.db.query(Restaurant) \
             .filter(Restaurant.kakaomap_id == id) \
             .first()
+
+        return restaurant
+
+    def update_number_of_reviews_and_raiting(self, data, state):
+        if state == "create":
+            restaurant_id = data["restaurant_id"]
+            star_input = data["star"]
+            restaurant = self.find_one(restaurant_id)
+            count_temp = restaurant.reviews
+            star_temp = restaurant.rating_avg
+            
+            if count_temp and count_temp != 0:
+                star_temp = star_temp * count_temp;
+                star_temp += star_input
+                count_temp += 1
+                star_temp = float(star_temp) / count_temp 
+            else:
+                count_temp = 1 
+                star_temp = star_input
+
+            data = {
+                    "reviews" :count_temp,
+                    "rating_avg":star_temp
+                }
+            self.update(restaurant_id,data)
+
+
+        elif state =="remove":
+            review = data
+            if review:
+                restaurant_id = review.restaurant_id
+                review_star = review.star
+                restaurant = self.find_one(restaurant_id)
+                if restaurant:
+                    restaurant_reviews = restaurant.reviews
+                    restaurant_star = restaurant.rating_avg
+
+                    star_temp = (restaurant_star * restaurant_reviews) - review_star
+                    count_temp = restaurant_reviews - 1
+                    if count_temp == 0:
+                        count_temp+=1
+
+                    star_temp = star_temp / count_temp
+
+                    data = {
+                        "reviews" :count_temp,
+                        "rating_avg":star_temp
+                    }
+                    self.update(restaurant_id,data)
+
 
         return restaurant
